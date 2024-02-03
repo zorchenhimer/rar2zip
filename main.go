f5918719d2c57fefe5d4d7b5a5c19c012a4fbcf0
Zorchenhimer <zorchenhimer@gmail.com>
2024-02-03 15:07:13 -0500

    Run gofmt

--- main.go
+++ main.go
@@ -1,72 +1,72 @@
 package main
 
 import (
-    "archive/zip"
-    "fmt"
-    "io"
-    "os"
-    "path/filepath"
-    "strings"
+	"archive/zip"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
 
-    "github.com/nwaples/rardecode"
+	"github.com/nwaples/rardecode"
 )
 
 var verbose bool = false
 
 func main() {
-    // Default to all rar files in the current directory.
-    if len(os.Args) == 1 {
-        os.Args = append(os.Args, "*.rar")
-    }
+	// Default to all rar files in the current directory.
+	if len(os.Args) == 1 {
+		os.Args = append(os.Args, "*.rar")
+	}
 
-    files := []string{}
-    for _, arg := range os.Args[1:len(os.Args)] {
-        if arg == "-v" || arg == "--verbose" {
-            verbose = true
-            fmt.Println("Verbose turned on")
-            continue
-        }
+	files := []string{}
+	for _, arg := range os.Args[1:len(os.Args)] {
+		if arg == "-v" || arg == "--verbose" {
+			verbose = true
+			fmt.Println("Verbose turned on")
+			continue
+		}
 
-        matches, err := filepath.Glob(arg)
-        if err != nil {
-            fmt.Printf("Glob failed: %s\n", err)
-            continue
-        }
-        if verbose {
-            for _, f := range matches {
-                fmt.Printf("Adding file %q\n", f)
-            }
-        }
-        files = append(files, matches...)
+		matches, err := filepath.Glob(arg)
+		if err != nil {
+			fmt.Printf("Glob failed: %s\n", err)
+			continue
+		}
+		if verbose {
+			for _, f := range matches {
+				fmt.Printf("Adding file %q\n", f)
+			}
+		}
+		files = append(files, matches...)
 
-        if len(matches) == 0 && exists(arg) {
-            if verbose {
-                fmt.Printf("Adding file: %q\n", arg)
-            }
-            files = append(files, arg)
-        }
-    }
+		if len(matches) == 0 && exists(arg) {
+			if verbose {
+				fmt.Printf("Adding file: %q\n", arg)
+			}
+			files = append(files, arg)
+		}
+	}
 
-    if len(files) == 0 {
-        fmt.Println("No files to process")
-        return
-    }
-    fmt.Printf("Number of files to process: %d\n", len(files))
+	if len(files) == 0 {
+		fmt.Println("No files to process")
+		return
+	}
+	fmt.Printf("Number of files to process: %d\n", len(files))
 
-    for _, f := range files {
+	for _, f := range files {
 		err := convert(f)
 		if err != nil {
 			fmt.Printf("Error converting %s: %s", f, err.Error())
 		}
-    }
+	}
 }
 
 func convert(inputname string) error {
 	outname := inputname
 	if strings.HasSuffix(outname, ".rar") {
-		outname = outname[:len(outname)-4]+".zip"
+		outname = outname[:len(outname)-4] + ".zip"
 	} else {
-		outname = outname+".zip"
+		outname = outname + ".zip"
 	}
 
 	rawin, err := os.Open(inputname)
@@ -104,8 +104,8 @@ func convert(inputname string) error {
 
 		fmt.Println(header.Name)
 		zhead := &zip.FileHeader{
-			Name: header.Name,
-			Method: zip.Deflate,
+			Name:     header.Name,
+			Method:   zip.Deflate,
 			Modified: header.ModificationTime,
 		}
 
@@ -124,8 +124,12 @@ func convert(inputname string) error {
 }
 
 func exists(path string) bool {
-    _, err := os.Stat(path)
-    if err == nil { return true }
-    if os.IsNotExist(err) { return false }
-    return true
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
